757fb758ee76d85b43591b03d7e75501bd41e0a9
Cameron Loukes <cameron.loukes@gmail.com>
2023-01-23 14:42:55 -0500

    Loading Message

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,8 +1,11 @@
 # Programmer: Cameron Loukes
 # Date: 1/23/23
-# Program:
+# Program: Infotech Center Upgrades
 
 """
-
+Our Welcome Screen will start our Program letting
+drivers know that the Infotech Center 4.0 OS is loading
 """
 
+print("\n\nWelcome - Infotech Center 4.0")
+print("\nInfotech Center 4.0 OS Loading")
