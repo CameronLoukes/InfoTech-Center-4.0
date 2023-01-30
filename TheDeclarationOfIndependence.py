5fc5c03cdad7145b3bdf0688cdda161d12c0670f
Cameron Loukes <cameron.loukes@gmail.com>
2023-01-30 13:28:13 -0500

    Small changes to Print Message such as color

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -10,7 +10,7 @@ drivers know that the Infotech Center 4.0 OS is loading
 import time  # I imported the time library for further use in code.
 import sys  # I imported the system library for further use in code.
 
-print('\n\033[1;34;48m Welcome to InfoTech Center 4.0')
+print('\n\033[1;36;40m Welcome to InfoTech Center 4.0')
 
 x = 0
 a = 0
@@ -27,4 +27,4 @@ while x != 20:
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\n\033[1;32;40m Infotech Center 4.0 OS is Loaded!')
