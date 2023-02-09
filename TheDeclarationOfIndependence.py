03dc7798d12e902fc688758e705438633edc729a
Cameron Loukes <cameron.loukes@gmail.com>
2023-02-09 13:53:04 -0500

    Defined Weather Function and called (Stable)

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -2,7 +2,20 @@
 # Date: 2/8/23
 # Program: Weather System Updates
 
-"""
+#Import Libraries here
+import random
 
-"""
+# Create weather conditions in a list and choose it randomly
+def weather():
+    weatherForcast = ["Snowing", "Blizzard", "Raining", "Hailing", "Foggy", "Windy", "Sunny"]
+    weatherCondition = random.choice(weatherForcast)
+    return weatherCondition
 
+#Variable to call weather() once in our VRS()
+weatherAlert = weather()
+
+print(weatherAlert)
+
+#VRS() to respond to the weather conditions
+def vehicleResponseSystem():
+    print("hey")
