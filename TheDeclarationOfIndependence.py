6fba05d0499a4bd2786561cf0688f453ef24244f
Cameron Loukes <cameron.loukes@gmail.com>
2023-02-16 13:40:59 -0500

    Created a list of drivers for the settings

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -3,6 +3,15 @@
 # Program:
 
 """
-
+I will create personalized driver settings for different drivers
 """
 
+# Import Libraries Here:
+import random
+
+def drivers():
+    listOfDrivers = ["Driver 1", "Driver 2", "Driver 3", "Driver 4"]
+    chooseDriver = random.choice(listOfDrivers)
+    return drivers
+
+# Create settings for drivers
