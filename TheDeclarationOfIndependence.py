a38c162fe7775388cbd13d2deca9f95c06f338ab
Cameron Loukes <cameron.loukes@gmail.com>
2023-02-16 13:58:55 -0500

    Finished lists for settings

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -8,10 +8,30 @@ I will create personalized driver settings for different drivers
 
 # Import Libraries Here:
 import random
+import time.sleep
 
 def drivers():
     listOfDrivers = ["Driver 1", "Driver 2", "Driver 3", "Driver 4"]
     chooseDriver = random.choice(listOfDrivers)
     return drivers
 
+def radio():
+    listOfRadioStations = ["Top Hits", "Pop", "HipHop", "Rock", "Country", "Jazz"]
+    chooseRadioStation = random.choice(listOfRadioStations)
+
+def steeringWheel():
+    listOfWheelPositions = ["Low", "Medium", "High"]
+    chooseWheelPosition = random.choice(listOfWheelPositions)
+
+def seat():
+    listOfSeatPositions = ["Close", "Middle", "Far"]
+    chooseSeatPosition = random.choice(listOfSeatPositions)
+
 # Create settings for drivers
+
+def driverSettings():
+    if "Driver 1":
+        print("Driver 1 has been chosen")
+    elif "Driver 2":
+    elif "Driver 3":
+    else "Driver 4":
